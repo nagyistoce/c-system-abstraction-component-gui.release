56f9c5a5f43aef328355173dd6ba5d48a35ebb93
d3x0r <d3x0r@localhost>
2010-09-07 04:09:05 -0700

    Backed out changeset: 54a8187a2e9a

---
+++ src/filesyslib/pathops.c
@@ -0,0 +1,262 @@
+
+#include <stdhdrs.h>
+#include <sack_types.h>
+#include <string.h>
+#include <filesys.h>
+#include <logging.h>
+#ifdef __LINUX__
+#include <time.h>
+#include <sys/stat.h>
+#endif
+//-----------------------------------------------------------------------
+
+FILESYS_NAMESPACE
+
+ CTEXTSTR  pathrchr ( CTEXTSTR path )
+{
+	CTEXTSTR end1, end2;
+	end1 = strrchr( path, '\\' );
+	end2 = strrchr( path, '/' );
+	if( end1 > end2 )
+		return end1;
+   return end2;
+}
+
+#ifdef __cplusplus
+ TEXTSTR  pathrchr ( TEXTSTR path )
+{
+	TEXTSTR end1, end2;
+	end1 = strrchr( path, '\\' );
+	end2 = strrchr( path, '/' );
+	if( end1 > end2 )
+		return end1;
+   return end2;
+}
+#endif
+
+//-----------------------------------------------------------------------
+
+ CTEXTSTR  pathchr ( CTEXTSTR path )
+{
+	CTEXTSTR end1, end2;
+	end1 = strchr( path, (int)'\\' );
+	end2 = strchr( path, (int)'/' );
+	if( end1 && end2 )
+	{
+		if( end1 < end2 )
+			return end1;
+      return end2;
+	}
+	else if( end1 )
+		return end1;
+	else if( end2 )
+      return end2;
+   return NULL;
+}
+
+//-----------------------------------------------------------------------
+
+TEXTSTR GetCurrentPath( TEXTSTR path, int len )
+{
+	if( !path )
+		return 0;
+#ifndef UNDER_CE
+#ifdef _WIN32
+	GetCurrentDirectory( len, path );
+#else
+	getcwd( path, len );
+#endif
+#endif
+	return path;
+}
+
+#ifndef _WIN32
+static void convert( P_64 outtime, time_t *time )
+{
+#warning convert time function is incomplete.
+	*outtime = *time;
+}
+#endif
+
+//-----------------------------------------------------------------------
+
+_64 GetTimeAsFileTime ( void )
+{
+#if defined( __LINUX__ )
+	struct timeval tmp;
+	struct timezone tz;
+	FILETIME result;
+	gettimeofday( &tmp, &tz );
+	result = ( tmp.tv_usec * 10LL ) + ( tmp.tv_sec * 1000LL * 1000LL * 10LL );
+	return result;
+#else
+	SYSTEMTIME st;
+	FILETIME result;
+	GetLocalTime( &st );
+	SystemTimeToFileTime( &st, &result );
+	return *(_64*)&result;
+#endif
+}
+
+ _64  GetFileWriteTime( CTEXTSTR name ) // last modification time.
+{
+#ifdef _WIN32
+	HANDLE hFile = CreateFile( name
+                                  , 0 // device access?
+                                  , FILE_SHARE_READ|FILE_SHARE_WRITE
+                                  , NULL
+                                  , OPEN_EXISTING
+                                  , 0
+                                  , NULL );
+	if( hFile != INVALID_HANDLE_VALUE )
+	{
+		FILETIME filetime;
+		//_64 realtime;
+		GetFileTime( hFile, NULL, NULL, &filetime );
+		CloseHandle( hFile );
+		//realtime = *(_64*)&filetime;
+		//realtime *= 100; // nano seconds?
+		return *(_64*)&filetime;
+	}
+	return 0;
+#else
+	struct stat statbuf;
+	_64 realtime;
+	stat( name, &statbuf );	
+	convert( &realtime, &statbuf.st_mtime );
+	return realtime;
+#endif	
+	return 0;
+}
+
+//-----------------------------------------------------------------------
+
+ LOGICAL  SetFileWriteTime( CTEXTSTR name, _64 filetime ) // last modification time.
+{
+#ifdef _WIN32
+	HANDLE hFile = CreateFile( name
+                                  , GENERIC_WRITE // device access?
+                                  , FILE_SHARE_READ|FILE_SHARE_WRITE
+                                  , NULL
+                                  , OPEN_EXISTING
+                                  , 0
+                                  , NULL );
+	if( hFile != INVALID_HANDLE_VALUE )
+	{
+		//_64 realtime;
+		SetFileTime( hFile, NULL, NULL, (CONST FILETIME*)&filetime );
+		CloseHandle( hFile );
+		//realtime = *(_64*)&filetime;
+	   //realtime *= 100; // nano seconds?
+      return TRUE;
+	}
+	return FALSE;
+#else
+	struct stat statbuf;
+	_64 realtime;
+	stat( name, &statbuf );	
+	convert( &realtime, &statbuf.st_mtime );
+	return realtime;
+#endif	
+	return 0;
+}
+
+//-----------------------------------------------------------------------
+
+ int  IsPath ( CTEXTSTR path )
+{
+	
+	if( !path )
+		return 0;
+#ifdef _WIN32
+	{
+		DWORD dwResult;
+		dwResult = GetFileAttributes( path );
+		if( dwResult == 0xFFFFFFFF )
+			return 0;
+		if( dwResult & FILE_ATTRIBUTE_DIRECTORY ) 
+			return 1;
+		return 0;
+	}
+#else
+        {
+	struct stat statbuf;
+	stat( path, &statbuf );
+        return S_ISDIR( statbuf.st_mode );
+        }
+#endif
+}
+
+//-----------------------------------------------------------------------
+
+ int  MakePath ( CTEXTSTR path )
+{
+	if( !path )
+		return 0;
+#ifdef _WIN32
+	return CreateDirectory( path, NULL );
+#else
+	return !mkdir( path, -1 ); // make directory with full umask permissions
+#endif
+}
+
+//-----------------------------------------------------------------------
+
+int  SetCurrentPath ( CTEXTSTR path )
+{
+	if( !path )
+		return 0;
+	lprintf( WIDE( "Set CurrentPath: %s" ), path );
+	SetDefaultFilePath( path );
+
+#ifndef UNDER_CE
+#ifdef _WIN32
+	return SetCurrentDirectory( path );
+#else
+	return !chdir( path );
+#endif
+#endif
+	return 1;
+}
+
+FILESYS_NAMESPACE_END
+
+
+//-----------------------------------------------------------------------
+// $Log: pathops.c,v $
+// Revision 1.14  2005/05/06 18:15:24  jim
+// Add ability to set file write time.
+//
+// Revision 1.13  2004/05/27 21:37:39  d3x0r
+// Syncpoint.
+//
+// Revision 1.12  2004/05/06 08:13:46  d3x0r
+// Apply repsiective const changes to pathchr, pathrchr
+//
+// Revision 1.11  2004/01/12 08:42:16  panther
+// Fix return type of pathchr
+//
+// Revision 1.10  2004/01/07 09:46:37  panther
+// Fix pathops to handle const char * decl.  Disable logging
+//
+// Revision 1.9  2003/08/12 12:14:01  panther
+// ...
+//
+// Revision 1.8  2003/04/21 20:02:31  panther
+// Support option to return file name only
+//
+// Revision 1.7  2003/01/31 16:23:24  panther
+// Cleaned for visual studio warnings
+//
+// Revision 1.6  2003/01/28 02:24:43  panther
+// Fixes to network - common timer for network pause... minor updates which should have been commited already
+//
+// Revision 1.5  2002/08/16 21:41:11  panther
+// Updated to compile under linux, also updated to new method of declaring
+// public externals.
+//
+// Revision 1.4  2002/07/26 09:16:55  panther
+// Added IsPath, CreatePath, SetCurrentPath, GetFileWriteTime
+// Modified GetCurrentPath
+//
+//
---
+++ src/msgsvr/summoner/construct.c
@@ -0,0 +1,144 @@
+#include <stdhdrs.h>
+#include <msgclient.h>
+#include <deadstart.h>
+#include <construct.h>
+#include <sqlgetoption.h>
+#include "summoner.h"
+
+#ifdef __cplusplus
+namespace sack { namespace task { namespace construct {
+using namespace sack::msg::client;
+#endif
+
+typedef struct local_tag
+{
+	_32 MsgBase;
+	TEXTCHAR my_name[256];
+	// handle a registry of external functions to call
+	// for alive checks... otherwise be simple and claim
+   // alive (at least the message services are alive)
+} LOCAL;
+
+static LOCAL l;
+
+static int CPROC HandleSummonerEvents( _32 SourceID, _32 MsgID, _32 *data, _32 len )
+{
+	switch( MsgID )
+	{
+	case MSG_RU_ALIVE:
+		// echo the data back to the server..
+		// this helps mate requests and responces...
+		SendRoutedServerMessage( SourceID, MSG_IM_ALIVE, data, len );
+		break;
+	case MSG_DIE:
+		lprintf( WIDE("Command to die, therefore I shall...") );
+		exit(0);
+		break;
+	default:
+		lprintf( WIDE("Received unknown message %") _32f WIDE(" from %") _32f WIDE(""), MsgID, SourceID );
+		break;
+	}
+   return TRUE;
+}
+
+//#if 0
+PRELOAD( Started )
+{
+#ifndef __NO_OPTIONS__
+	if( SACK_GetProfileIntEx( "SACK/Summoner", "Auto register with summoner?", 0, TRUE ) )
+#else
+   if( 0 )
+#endif
+	{
+		l.MsgBase = LoadServiceEx( SUMMONER_NAME, HandleSummonerEvents );
+		//lprintf( WIDE("Message base for service is %d"), l.MsgBase );
+		if( l.MsgBase != INVALID_INDEX )
+		{
+			_32 result, result_length;
+			result_length = sizeof( l.my_name );
+			if( !TransactServerMessage( l.MsgBase + MSG_WHOAMI, NULL, 0
+											  , &result, l.my_name, &result_length ) )
+			{
+				// since we JUST loaded it, this shold be nearly impossible to hit.
+				lprintf( WIDE("Failed to find out who I am from summoner.") );
+				UnloadService( l.MsgBase );
+				l.MsgBase = INVALID_INDEX;
+				return;
+			}
+			else if( result != ((MSG_WHOAMI+l.MsgBase)|SERVER_SUCCESS ) )
+			{
+				lprintf( WIDE("Server responce was in error... disable support") );
+				UnloadService( l.MsgBase );
+				l.MsgBase = INVALID_INDEX;
+				return;
+			}
+			else if( !result_length )
+			{
+				lprintf( WIDE("Summoner is not responsible for us, and requires no notifications." ) );
+				UnloadService( l.MsgBase );
+				l.MsgBase = INVALID_INDEX;
+				return;
+			}
+			//else l.my_name is my task name from sommoner.config
+			if( !TransactServerMessage( l.MsgBase + MSG_IM_STARTING, l.my_name, (_32)strlen( l.my_name ) + 1
+											  , NULL, NULL, 0 ) )
+			{
+				// this should almost be guaranteed to work...
+				lprintf( WIDE("Failed to send starting to summoner... disable support") );
+				UnloadService( l.MsgBase );
+				l.MsgBase = INVALID_INDEX;
+				return;
+			}
+			//lprintf( WIDE("We're starting, go ahead.") );
+		}
+	}
+	else
+		l.MsgBase = INVALID_INDEX;
+
+}
+//#endif
+
+ void  LoadComplete ( void )
+{
+	_32 result;
+   // if we registered with the summoner...
+	if( l.MsgBase != INVALID_INDEX )
+	{
+		//lprintf( WIDE("Sending IM_READY to summoner...\n") );
+		result = ((l.MsgBase+MSG_IM_READY) | SERVER_SUCCESS);
+		if( TransactServerMessage( l.MsgBase + MSG_IM_READY, l.my_name, (_32)strlen( l.my_name ) + 1
+										 , NULL /*&result*/, NULL, 0 )
+		  )
+		{
+			if( result == ((l.MsgBase+MSG_IM_READY) | SERVER_SUCCESS) )
+			{
+			}
+			else
+			{
+				lprintf( WIDE("Summoner has somehow complained that we're started?!") );
+				DebugBreak();
+			}
+		}
+		else
+		{
+			lprintf( WIDE("Summoner has dissappeared.  Disabling support.") );
+			l.MsgBase = 0;
+		}
+	}
+	//else
+   //   lprintf( WIDE("Service has been disabled.") );
+}
+
+ATEXIT( Ended )
+{
+	if( l.MsgBase != INVALID_INDEX )
+	{
+		lprintf( WIDE("mark ready in summoner (dispatch as ended?)") );
+		LoadComplete();
+		UnloadService( l.MsgBase );
+	}
+}
+
+#ifdef __cplusplus
+}}} //namespace sack namespace 
+#endif
