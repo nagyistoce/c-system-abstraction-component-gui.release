884040705a7807ada5e4e19423e559b335b04944
d3x0r <d3x0r@localhost>
2010-09-18 02:48:03 -0700

    Drop obsolete codes

--- src/apps/video_link/scripts/ianson.bat
+++
@@ -1,9 +0,0 @@
-if %1==display goto wait
-
-echo !update link_hall_state join location on hall_id=location.ID set task_launched=0 where packed_name='fortunet-pc' | sqlcmd vsrvr
-goto done
-:wait
-echo !update link_hall_state join location on hall_id=location.ID set task_launched=0 where packed_name='fortunet-pc' | sqlcmd vsrvr
-echo !update link_hall_state join location on hall_id=location.ID set participating=1 where packed_name='fortunet-pc' | sqlcmd vsrvr
-pause
-:done
--- src/apps/video_link/scripts/scpall.bat
+++
@@ -1,6 +0,0 @@
-pscp %1 root@10.4.50.20:/usr/src/FortuNet
-pscp %1 root@10.4.40.20:/usr/src/FortuNet
-pscp %1 root@10.4.20.20:/usr/src/FortuNet
-pscp %1 root@10.4.245.20:/usr/src/FortuNet
-pscp %1 root@10.4.70.20:/usr/src/FortuNet
-pscp %1 root@10.4.60.20:/usr/src/FortuNet
