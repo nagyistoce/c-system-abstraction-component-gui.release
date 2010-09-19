9988659d5654d3c23c876a7aec8661c181f5fd6b
d3x0r <d3x0r@localhost>
2010-09-19 16:57:33 -0700

    Add an editor, and update deploy project, and add cmakelists

--- src/apps/video_link/video_deploy/deploy.c
+++ src/apps/video_link/video_deploy/deploy.c
@@ -1,5 +1,6 @@
 #include <stdhdrs.h>
 #include <configscript.h>
+#include <network.h>
 #include <controls.h>
 enum {
 	LISTBOX_ITEMS,
@@ -10,8 +11,10 @@ struct site_info
 {
 	CTEXTSTR name;
 	CTEXTSTR hostname;
+	CTEXTSTR host_address;
 	int hosts_sql;
-	CTEXTSTR sql_server;
+	CTEXTSTR mysql_server;
+
    CTEXTSTR address;
 
 };
@@ -19,14 +22,14 @@ struct site_info
 struct map_file
 {
 	CTEXTSTR name;
-   CTEXTSTR filename;
+	CTEXTSTR filename;
 };
 
 static struct {
 	PLIST maps; // list of struct map_file
 	int nMaps;
 
-	PLIST sites; // list of struct map_file
+	PLIST sites; // list of struct site_info
 
 	struct map_file *selected_map;
    struct site_info *selected_site;
@@ -59,15 +62,16 @@ void CPROC FoundMap( PTRSZVAL psv, CTEXTSTR name, int flags )
 
 void SelectMap( void )
 {
-   void *info = NULL;
-	while( ScanFiles( ".", "*.Map", &info, FoundMap, 0, 0 ) );
+	void *info = NULL;
+   TEXTCHAR tmp[256];
+	while( ScanFiles( GetFileGroupText( GetFileGroup( "Configuration Maps", "../resources" ), tmp, sizeof( tmp ) ), "*.Map", &info, FoundMap, 0, 0 ) );
 
    if( l.nMaps > 1 )
 	{
 		int okay = 0;
       int done = 0;
 		PSI_CONTROL frame = CreateFrame( "Select Site Map", -1, -1, 480, 320, BORDER_NORMAL, NULL );
-		PSI_CONTROL listbox = MakeNamedControl( frame, LISTBOX_CONTROL_NAME, 5, 5, 470, 295, 0 );
+		PSI_CONTROL listbox = MakeNamedControl( frame, LISTBOX_CONTROL_NAME, 5, 5, 470, 285, 0 );
 		AddCommonButtons( frame, &done, &okay );
 		{
 			struct map_file *map;
@@ -85,6 +89,7 @@ void SelectMap( void )
 			PTRSZVAL psv_map = GetItemData( pli );
          l.selected_map = (struct map_file *)psv_map;
 		}
+      DestroyFrame( &frame );
 	}
 	else
       l.selected_map = (struct map_file*)GetLink( &l.maps, 0 );
@@ -99,7 +104,7 @@ void SelectSite( void )
 		int okay = 0;
       int done = 0;
 		PSI_CONTROL frame = CreateFrame( "Select Site", -1, -1, 480, 320, BORDER_NORMAL, NULL );
-		PSI_CONTROL listbox = MakeNamedControl( frame, LISTBOX_CONTROL_NAME, 5, 5, 470, 295, 0 );
+		PSI_CONTROL listbox = MakeNamedControl( frame, LISTBOX_CONTROL_NAME, 5, 5, 470, 285, 0 );
 		AddCommonButtons( frame, &done, &okay );
 		{
 			struct site_info *site;
@@ -117,6 +122,7 @@ void SelectSite( void )
 			PTRSZVAL psv_site = GetItemData( pli );
          l.selected_site = (struct site_info *)psv_site;
 		}
+      DestroyFrame( &frame );
 	}
 }
 
@@ -175,14 +181,59 @@ void ReadMap( void )
    DestroyConfigurationHandler( pch );
 }
 
+void UpdateHostsFile( void )
+{
+	CTEXTSTR root = OSALOT_GetEnvironmentVariable( "SystemRoot" );
+	TEXTCHAR tmp[256];
+   FILE *file;
+	snprintf( tmp, sizeof( tmp ), "%s/system32/drivers/etc/hosts", root );
+	file = fopen( tmp, "wt" );
+	fprintf( file, "# Copyright (c) 1993-2009 Microsoft Corp.\n" );
+	fprintf( file, "#\n" );
+	fprintf( file, "# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.\n" );
+	fprintf( file, "#\n" );
+	fprintf( file, "# This file contains the mappings of IP addresses to host names. Each\n" );
+	fprintf( file, "# entry should be kept on an individual line. The IP address should\n" );
+	fprintf( file, "# be placed in the first column followed by the corresponding host name.\n" );
+	fprintf( file, "# The IP address and the host name should be separated by at least one\n" );
+	fprintf( file, "# space.\n" );
+	fprintf( file, "#\n" );
+	fprintf( file, "# Additionally, comments (such as these) may be inserted on individual\n" );
+	fprintf( file, "# lines or following the machine name denoted by a '#' symbol.\n" );
+	fprintf( file, "#\n" );
+	fprintf( file, "# For example:\n" );
+	fprintf( file, "#\n" );
+	fprintf( file, "#      102.54.94.97     rhino.acme.com          # source server\n" );
+	fprintf( file, "#       38.25.63.10     x.acme.com              # x client host\n" );
+	fprintf( file, "\n" );
+	fprintf( file, "# localhost name resolution is handled within DNS itself.\n" );
+	fprintf( file, "#	127.0.0.1       localhost\n" );
+	fprintf( file, "#	::1             localhost\n" );
+	fprintf( file, "\n" );
+	{
+		INDEX idx;
+		struct site_info *site;
+		LIST_FORALL( l.sites, idx, struct site_info *, site )
+		{
+			fprintf( file, "%s %s vsrvr.%s bdata.%s\n"
+					 , l.selected_site->host_address
+					 , l.selected_site->hostname
+					 , l.selected_site->hostname
+					 , l.selected_site->hostname
+					 );
+		}
+	}
+}
+
 void UpdateConfiguration( void )
 {
 	// based on selected map and site, set hostname, and ODBC connection parameters.
 	if( StrCaseCmp( GetSystemName(), l.selected_site->hostname ) )
 	{
 		lprintf( "hostname needs update" );
-
 	}
+
+   UpdateHostsFile();
 
 
 
@@ -198,6 +249,8 @@ int main( void )
 
    if( l.selected_site )
 		UpdateConfiguration();
-
+	else
+      SimpleMessageBox( NULL, "Configuration Canceled", "System setup may not be complete." );
    return 0;
 }
+
