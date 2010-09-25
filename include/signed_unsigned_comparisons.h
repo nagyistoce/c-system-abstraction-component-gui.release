4f3d083caa9cec2aa5d6886734589691c94a3400
d3x0r <d3x0r@localhost>
2010-09-24 21:57:45 -0700

    Begin implementing SUS and USS macros for signed-unsigned and unsigned-signed comparisons.  Since compilers can't assure me that they will do it right, and will instead throw warnings.... I guess I have to do it for them

---
+++ include/signed_unsigned_comparisons.h
@@ -0,0 +1,33 @@
+
+#ifdef _MSC_VER
+#define COMPILER_THROWS_SIGNED_UNSIGNED_MISMATCH
+#endif
+
+#ifdef COMPILER_THROWS_SIGNED_UNSIGNED_MISMATCH
+
+#define SUS_GT(a,at,b,bt)   (((a)<0)?0:(((bt)a)>(b)))
+#define USS_GT(a,at,b,bt)   (((b)<0)?1:((a)>((at)b)))
+
+#define SUS_LT(a,at,b,bt)   (((a)<0)?1:(((bt)a)<(b)))
+#define USS_LT(a,at,b,bt)   (((b)<0)?0:((a)<((at)b)))
+
+#define SUS_GTE(a,at,b,bt)  (((a)<0)?0:(((bt)a)>=(b)))
+#define USS_GTE(a,at,b,bt)  (((b)<0)?1:((a)>=((at)b)))
+
+#define SUS_LTE(a,at,b,bt)  (((a)<0)?1:(((bt)a)<=(b)))
+#define USS_LTE(a,at,b,bt)  (((b)<0)?0:((a)<=((at)b)))
+
+
+#else
+#define SUS_GT(a,b)   ((a)>(b))
+#define USS_GT(a,b)   ((a)>(b))
+
+#define SUS_LT(a,b)   ((a)<(b))
+#define USS_LT(a,b)   ((a)<(b))
+
+#define SUS_GTE(a,b)  ((a)>=(b))
+#define USS_GTE(a,b)  ((a)>=(b))
+
+#define SUS_LTE(a,b)  ((a)<=(b))
+#define USS_LTE(a,b)  ((a)<=(b))
+#endif
