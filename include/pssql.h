a962abba64d515c1680692f143990b78cbe3375c
d3x0r <d3x0r@localhost>
2010-09-29 16:31:44 -0700

    visual studio can use va_args for building keycolumns this way

--- include/pssql.h
+++ include/pssql.h
@@ -186,7 +186,7 @@ typedef struct required_field_tag
 	CTEXTSTR previous_names[MAX_PREVIOUS_FIELD_NAMES];
 } FIELD, *PFIELD;
 
-#ifndef _MSC_VER
+#if !define( _MSC_VER ) || ( _MSC_VER >= 800 )
 /* A macro to append a NULL automatically to a list of strings.
    Example
    <code lang="c++">
