64fc8fd6eca40fe37f19f93bb34cd4b4c564ac96
d3x0r <d3x0r@localhost>
2010-09-30 00:09:12 -0700

    Add a batchfile which mass-builds install for sack_core

---
+++ vs10_all.bat
@@ -0,0 +1,12 @@
+mkdir Debug
+cd Debug
+cmake -G "Visual Studio 10"  \sack -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_core
+devenv sack.sln /Build debug /Project Install.vcxproj
+cd ..
+
+mkdir Release
+cd Release
+cmake -G "Visual Studio 10"  \sack -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_core
+devenv sack.sln /Build release /Project Install.vcxproj
+cd ..
+
