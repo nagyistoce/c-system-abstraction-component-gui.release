2b6e62e73b73533a424dd6a93beacf3a6076d60d
d3x0r <d3x0r@localhost>
2010-09-30 21:23:14 -0700

    Define where vlc is

--- vs10_all.bat
+++ vs10_all.bat
@@ -13,28 +13,28 @@ cd ..
 cmd /c "M:\build\vs10_sack_core\Debug\sack_deploy.exe"
 mkdir binary_Debug
 cd binary_Debug
-cmake -G "Visual Studio 10"  \sack\binary -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_core_binaries
+cmake -G "Visual Studio 10"  \sack\binary -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_core_binaries_debug
 devenv SackBinaries.sln /Build debug /Project Install.vcxproj
 cd ..
 
 cmd /c "M:\build\vs10_sack_core\Release\sack_deploy.exe"
 mkdir binary_Release
 cd binary_Release
-cmake -G "Visual Studio 10"  \sack\binary -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_binaries
+cmake -G "Visual Studio 10"  \sack\binary -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_binaries_release
 devenv SackBinaries.sln /Build release /Project Install.vcxproj
 cd ..
 
 cmd /c "M:\build\vs10_sack_core\Debug\sack_deploy.exe"
 mkdir intershell_Debug
 cd intershell_Debug
-cmake -G "Visual Studio 10"  \sack\src\InterShell -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_intershell
+cmake -G "Visual Studio 10"  \sack\src\InterShell -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_intershell_debug -DVLC_PATH="e:/tools/vlc-1.1.4"
 devenv InterShell.sln /Build debug /Project Install.vcxproj
 cd ..
 
 cmd /c "M:\build\vs10_sack_core\Release\sack_deploy.exe"
 mkdir intershell_Release
 cd intershell_Release
-cmake -G "Visual Studio 10"  \sack\src\InterShell -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_intershell
+cmake -G "Visual Studio 10"  \sack\src\InterShell -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=M:/build/vs10_sack_intershell_release -DVLC_PATH="e:/tools/vlc-1.1.4"
 devenv InterShell.sln /Build release /Project Install.vcxproj
 cd ..
 
