5e86754950ade8e93ae90736e4d9be17f12c50da
Jim <Jim@localhost>
2010-10-04 15:57:33 -0700

    Supprot setting private or non private options

--- src/SQLlib/optlib/setoptions/setoption.c
+++ src/SQLlib/optlib/setoptions/setoption.c
@@ -4,15 +4,19 @@
 
 int main( int argc, char **argv )
 {
-	if( argc < 4 )
+	if( argc < 5 )
 	{
-		printf( "Usage: %s [path] [option] [value]\n", argv[0] );
-		printf( "   path = \"/flashdrive.ini/eltanin receiver\"\n" );
+		printf( "Usage: %s [root_name] [path] [option] [value]\n", argv[0] );
+		printf( "   root_name = \"flashdrive.ini\" (if \"\" then option will go under DEFAULT \n" );
+		printf( "   path = \"eltanin receiver\"\n" );
 		printf( "   option = \"Bonanza Enable\"\n" );
 		printf( "   value = 0/1\n" );
       printf( "  %s \"/flashdrive.ini/eltanin receiver\" \"Bonanza Enable\" 1\n", argv[0] );
       return 0;
 	}
-   SACK_WriteProfileString( argv[1], argv[2], argv[3] );
+   if( argv[1][0] )
+		SACK_WritePrivateProfileString( argv[2], argv[3], argv[4], argv[1] );
+   else
+		SACK_WriteProfileString( argv[2], argv[3], argv[4] );
    return 0;
 }
